084a88e31a1adf5613ff15c3f34fba6c4b14602b
Pasha.azd <ppasha.azadeh@gmail.com>
2022-11-20 20:15:47 +0330

    CREATE movies_detail.html -- DEFINE movie_detail view -- CONFIGURE a urlpattern for movie_detail view

--- movies/urls.py
+++ movies/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from movies.views import movies_list
+from movies.views import movies_list, movie_detail
 
 urlpatterns = [
-    path('', movies_list, name='movies_list')
+    path('', movies_list, name='movies_list'),
+    path('movies/<int:pk>', movie_detail, name='movie_detail'),
 ]
--- movies/views.py
+++ movies/views.py
@@ -9,3 +9,17 @@ def movies_list(request):
         'movies': movies
     }
     return render(request, 'movies/movies_list.html', context=context)
+
+
+def movie_detail(request, pk):
+    movie = Movie.objects.get(id=pk)
+    genre = movie.genres.all()
+    crew = movie.crew.all()
+
+    context = {
+        'movie': movie,
+        'genre': genre,
+        'crew': crew
+    }
+
+    return render(request, 'movies/movie_detail.html', context=context)
