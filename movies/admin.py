7b84aa94d6fdb191a598032f85aa4a81b444fe73
amirehsani <amirmohammad7284@gmail.com>
2022-11-20 11:35:03 +0330

    admin panel created
    TODO: more specifications needs to be done

--- movies/admin.py
+++ movies/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from movies.models import *
 
-# Register your models here.
+admin.site.register(Genre)
+admin.site.register(Role)
+admin.site.register(Crew)
+admin.site.register(Movie)
+admin.site.register(MovieCrew)
--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,87 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
-# Create your models here.
+
+class Genre(models.Model):
+    title = models.CharField(max_length=50)
+    is_valid = models.BooleanField(default=True)
+    created_time = models.DateTimeField(auto_now_add=True)
+    modified_time = models.DateTimeField(auto_now=True)
+
+    # auto_now_add: the time the instance was created
+    # auto_now: the time the instance was modified
+
+    def __str__(self):
+        return self.title
+    # the magic method that lets us see the results by movie TITLE
+
+
+class Role(models.Model):
+    title = models.CharField(max_length=50)
+    is_valid = models.BooleanField(default=True)
+    created_time = models.DateTimeField(auto_now_add=True)
+    modified_time = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Crew(models.Model):
+    MALE = 1
+    FEMALE = 2
+    GENDER_CHOICES = (
+        (MALE, 'Male'),
+        (FEMALE, 'Female')
+    )
+    # we used a tuple here because the dataset should be iterable (can also use lists)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    birthday = models.DateField(null=True, blank=True)
+    gender = models.PositiveSmallIntegerField(choices=GENDER_CHOICES, default=MALE)
+    avatar = models.ImageField(upload_to='crew/avatars/', null=True, blank=True)
+    is_valid = models.BooleanField(default=True)
+    created_time = models.DateTimeField(auto_now_add=True)
+    modified_time = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+    release_date = models.DateField(null=True, blank=True)
+    avatar = models.ImageField(upload_to='movies/avatars/', null=True, blank=True)
+    genres = models.ManyToManyField(Genre)
+    crew = models.ManyToManyField(Crew, through='MovieCrew')
+    is_valid = models.BooleanField(default=True)
+    created_time = models.DateTimeField(auto_now_add=True)
+    modified_time = models.DateTimeField(auto_now=True)
+
+    # def get_description(self):
+    #    return self.description.lower()
+
+    def __str__(self):
+        return self.title
+
+    # show the object by its title
+
+    def clean(self):
+        raise ValidationError('Not good')
+
+
+class MovieCrew(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    crew = models.ForeignKey(Crew, on_delete=models.CASCADE)
+    role = models.ForeignKey(Role, on_delete=models.CASCADE)
+    created_time = models.DateTimeField(auto_now_add=True)
+    modified_time = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        unique_together = ('movie', 'crew', 'role')
+
+    # Django unique_together is used to make two or more model fields to be unique
+
+    # https://www.geeksforgeeks.org/meta-class-in-models-django/
+
+    # Meta classes are used to add or change a behavior in your class
