6480d054e0dcb9b702413ae78bb244a8321ef475
Pasha.azd <ppasha.azadeh@gmail.com>
2022-11-20 19:46:58 +0330

    MODIFY movies.urls and movies.views

--- movies/urls.py
+++ movies/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 
+from movies.views import movies_list
+
 urlpatterns = [
-    # path('', movies_list, name='movies_list')
+    path('', movies_list, name='movies_list')
 ]
--- movies/views.py
+++ movies/views.py
@@ -3,9 +3,9 @@ from django.shortcuts import render
 from movies.models import Movie
 
 
-# def movies_list(request):
-#     movies = Movie.objects.all()
-#     context = {
-#         'movies': movies
-#     }
-#     return render(request, 'movies/movies_list.html', context=context)
+def movies_list(request):
+    movies = Movie.objects.all()
+    context = {
+        'movies': movies
+    }
+    return render(request, 'movies/movies_list.html', context=context)
