9750bcf239b19748f04d88cfd703c43ff16bc246
Pasha.azd <ppasha.azadeh@gmail.com>
2022-11-21 16:45:52 +0330

    Add some user authentication logic

--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
 
 from movies.models import Movie
@@ -5,8 +6,13 @@ from movies.models import Movie
 
 def movies_list(request):
     movies = Movie.objects.all()
+    if User.is_authenticated:
+        user = True
+    else:
+        user = False
     context = {
-        'movies': movies
+        'movies': movies,
+        'User': user
     }
     return render(request, 'movies/movies_list.html', context=context)
 
@@ -15,11 +21,17 @@ def movie_detail(request, pk):
     movie = Movie.objects.get(id=pk)
     genre = movie.genres.all()
     crew = movie.crew.all()
+    user = User.is_authenticated
 
     context = {
         'movie': movie,
         'genre': genre,
-        'crew': crew
+        'crew': crew,
+        'User': user
     }
 
     return render(request, 'movies/movie_detail.html', context=context)
+
+
+def movie_add(request):
+    pass
