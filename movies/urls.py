0df66987d0d4d27a8cf956d571e76194dede117b
Pasha.azd <ppasha.azadeh@gmail.com>
2022-11-24 12:39:44 +0330

    ADD edit_movie and delete_movie urls in movies.urls

--- movies/urls.py
+++ movies/urls.py
@@ -2,12 +2,14 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
 
-from movies.views import movies_list, movie_detail, movie_add
+from movies.views import movies_list, movie_detail, movie_add, edit_movie, delete_movie
 
 urlpatterns = [
     path('', movies_list, name='movies_list'),
     path('movies/<int:pk>', movie_detail, name='movie_detail'),
     path('movies/add', movie_add, name='movie_add'),
+    path('edit_movie/<int:pk>/', edit_movie, name="edit_movie"),
+    path('delete_movie/<int:pk>/', delete_movie, name="delete_movie"),
 ]
 
 if settings.DEBUG:
