4a0cd9a30a4e54345488b9d34aed79fae7f64ea7
Pasha.azd <ppasha.azadeh@gmail.com>
2022-11-25 14:58:02 +0330

    ADD movie_comment form -- ADD movies/comment url

--- movies/urls.py
+++ movies/urls.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
 
-from movies.views import movies_list, movie_detail, movie_add, edit_movie, delete_movie
+from movies.views import movies_list, movie_detail, movie_add, edit_movie, delete_movie, movie_comment
 
 urlpatterns = [
     path('', movies_list, name='movies_list'),
@@ -10,6 +10,7 @@ urlpatterns = [
     path('movies/add', movie_add, name='movie_add'),
     path('edit_movie/<int:pk>/', edit_movie, name="edit_movie"),
     path('delete_movie/<int:pk>/', delete_movie, name="delete_movie"),
+    path('movies/<int:pk>/comment', movie_comment, name='movie_comment')
 ]
 
 if settings.DEBUG:
--- movies/views.py
+++ movies/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect, get_object_or_404
 
-from movies.forms import MovieAddForm
+from movies.forms import MovieAddForm, CommentForm
 from movies.models import Movie, MovieCrew
 
 
@@ -22,13 +22,14 @@ def movie_detail(request, pk):
     directors = []
     writers = []
     actors = []
+
     for crew in movie_crew:
         if crew.role.title == 'Director':
-            directors.append(str(crew.crew.first_name) + str(crew.crew.last_name))
+            directors.append(crew.crew.full_name())
         elif crew.role.title == 'Writer':
-            writers.append(str(crew.crew.first_name) + str(crew.crew.last_name))
+            writers.append(crew.crew.full_name())
         elif crew.role.title == 'Actor':
-            actors.append(str(crew.crew.first_name) + str(crew.crew.last_name))
+            actors.append(crew.crew.full_name())
 
     context = {'movie': movie,
                'Directors': directors,
@@ -94,3 +95,32 @@ def delete_movie(request, pk):
     movie.save()
 
     return redirect('movies_list')
+
+
+def movie_comment(request, pk=None, comment_form=None):
+    if request.method == 'GET':
+        if not comment_form:
+            comment_form = CommentForm()
+
+        context_get = {
+            'pk': pk,
+            'form': comment_form
+        }
+
+        return render(request, 'movies/comment.html', context=context_get)
+
+    if request.method == 'POST':
+        comment_form = CommentForm(request.POST)
+        if request.user.is_authenticated:
+            if comment_form.is_valid():
+                comment_form.save(commit=False)
+                comment_form.user = request.user
+                comment_form.movie = pk
+                comment_form.save()
+            else:
+                redirect('movie_comment', comment_form)
+
+        else:
+            return redirect('movie_comment', comment_form)
+
+    return redirect('movie_detail')
