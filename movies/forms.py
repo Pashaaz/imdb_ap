870fa5b2435384eefddc9f81701569c0bc61905a
Pasha.azd <ppasha.azadeh@gmail.com>
2022-11-25 14:53:08 +0330

    CREATE CommentForm in movies.forms

--- movies/forms.py
+++ movies/forms.py
@@ -1,9 +1,15 @@
 from django import forms
 
-from movies.models import Movie
+from movies.models import Movie, MovieComment
 
 
 class MovieAddForm(forms.ModelForm):
     class Meta:
         model = Movie
         fields = ['title', 'description', 'release_date', 'avatar', 'genres']
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = MovieComment
+        fields = ['text']
